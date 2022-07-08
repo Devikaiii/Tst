2aa0412dba18697c26df9a8105c3464babee1482
BELUGA_XD <96910602+belugaop@users.noreply.github.com>
2022-07-08 21:38:02 +0530

    Update filters_mdb.py

--- database/filters_mdb.py
+++ database/filters_mdb.py
@@ -120,3 +120,20 @@ async def filter_stats():
     totalcollections = len(collections)
 
     return totalcollections, totalcount
+
+
+
+def unpack_new_file_id(new_file_id):
+    """Return file_id, file_ref"""
+    decoded = FileId.decode(new_file_id)
+    file_id = encode_file_id(
+        pack(
+            "<iiqq",
+            int(decoded.file_type),
+            decoded.dc_id,
+            decoded.media_id,
+            decoded.access_hash
+        )
+    )
+    file_ref = encode_file_ref(decoded.file_reference)
+    return file_id, file_ref
